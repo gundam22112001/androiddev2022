d05f3b8f992733b649cf0c9e7b6cb8b01bd1b25c
gundam22112001 <gundam22112001@gmai.com>
2021-10-30 15:31:23 +0700

    add Practical Work 15

--- usthweather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ usthweather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -6,22 +6,37 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
     private TabLayout tab;
     private ViewPager pager;
     private ProgressBar progressBar;
+    private static final String downloadUrl = "https://usth.edu.vn/uploads/logo_moi-eng.png";
+    private ImageView logo;
 //    Toolbar toolbar;
 
     @Override
@@ -69,8 +84,8 @@ public class WeatherActivity extends AppCompatActivity {
 //                        handler.sendMessage(msg);
 //                    }
 //                });
-//                t.start();
-                refreshAsyncTask(progressBar);
+//                refreshAsyncTask(progressBar);
+                new imageDownloader().execute(downloadUrl);
                 return true;
             case R.id.action_setting:
                 openPreActivity();
@@ -80,55 +95,89 @@ public class WeatherActivity extends AppCompatActivity {
         }
     }
 
-//    final Handler handler = new Handler(Looper.getMainLooper()) {
+//    public void refreshAsyncTask(View v) {
+//        AsyncTask task = new refresh();
+//        task.execute(5);
+//    }
+//
+//    private class refresh extends android.os.AsyncTask<Integer,Integer, String> {
+//
 //        @Override
-//        public void handleMessage(@NonNull Message msg) {
-//            String content = msg.getData().getString("server_response");
-//            Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//
+//            progressBar.setVisibility(View.VISIBLE);
 //        }
-//    };
+//        @Override
+//        protected String doInBackground(Integer... integers) {
+//            for (int i = 0; i <= integers[0]; i++){
+//                publishProgress((i * 100)/integers[0]);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return "Finish Refreshing!";
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Integer... values) {
+//            super.onProgressUpdate(values);
+//
+//            progressBar.setProgress(values[0]);
+//        }
+//        @Override
+//        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+//
+//            Toast.makeText(WeatherActivity.this, s, Toast.LENGTH_SHORT).show();
+//            progressBar.setVisibility(View.INVISIBLE);
+//        }
+//    }
+//
+//
 
-
-    public void refreshAsyncTask(View v) {
-        AsyncTask task = new AsyncTask();
-        task.execute(5);
-    }
-
-    private class AsyncTask extends android.os.AsyncTask<Integer,Integer, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressBar.setVisibility(View.VISIBLE);
-        }
-        @Override
-        protected String doInBackground(Integer... integers) {
-            for (int i = 0; i < integers[0]; i++){
-                publishProgress((i * 100)/integers[0]);
+        private class imageDownloader extends AsyncTask<String, Void, Bitmap> {
+            HttpURLConnection connection;
+            Bitmap bitmap;
+            @Override
+            protected Bitmap doInBackground(String... strings) {
                 try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                    URL url = new URL(strings[0]);
+                    connection =(HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setDoInput(true);
+                    connection.connect();
+                    InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                } catch (MalformedURLException e) {
                     e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    connection.disconnect();
+                }
+                return bitmap;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                if (bitmap != null){
+                    logo =(ImageView) findViewById(R.id.image);
+                    logo.setImageBitmap(bitmap);
+                    Toast.makeText(getApplicationContext(), "Download Successful", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Download Failed", Toast.LENGTH_SHORT).show();
                 }
             }
-            return "Finish Refreshing!";
-        }
 
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-            progressBar.setProgress(values[0]);
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                super.onProgressUpdate(values);
+            }
         }
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            Toast.makeText(WeatherActivity.this, s, Toast.LENGTH_SHORT).show();
-            progressBar.setVisibility(View.INVISIBLE);
-        }
-    }
 
     @Override
     protected void onStart() {
